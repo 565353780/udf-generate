1495f879af8c65292e06dee3cf5fbb6158968423
chLi <565353780@qq.com>
2022-10-09 20:09:36 +0800

    add new func, simplify getFilePath call algo

--- udf_generate/Method/paths.py
+++ udf_generate/Method/paths.py
@@ -16,9 +16,20 @@ def createFileFolder(file_path):
     return True
 
 
-def getFilePath(file_basepath, param_name_value_list, file_format):
+def getFilePath(file_basepath, param_name_value_list=[], file_format="npy"):
     file_path = file_basepath
     for param_name_value in param_name_value_list:
         file_path += "_" + param_name_value[0] + "_" + str(param_name_value[1])
     file_path += "." + file_format
     return file_path
+
+
+def renameFile(source_file_path, target_file_path):
+    assert os.path.exists(source_file_path)
+
+    while os.path.exists(source_file_path):
+        try:
+            os.rename(source_file_path, target_file_path)
+        except:
+            continue
+    return True
