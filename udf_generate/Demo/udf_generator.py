ea65082c445343437c350bb79f02118a7c3a7549
chLi <565353780@qq.com>
2022-10-09 19:29:34 +0800

    update mesh path

--- udf_generate/Demo/udf_generator.py
+++ udf_generate/Demo/udf_generator.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 from udf_generate.Module.udf_generator import UDFGenerator
 
 
 def demo():
-    mesh_file_path = "/home/chli/scan2cad/im3d/data/pix3d/metadata/model/bed/IKEA_BEDDINGE/model.obj"
-    udf_save_file_basepath = "/home/chli/scan2cad/im3d_udf/bed/IKEA_BEDDINGE/udf"
+    mesh_file_path = \
+        "/home/chli/chLi/ShapeNet/Core/ShapeNetCore.v2/02691156/1a04e3eab45ca15dd86060f189eb133/models/model_normalized.obj"
+    udf_save_file_basepath = "/home/chli/chLi/ShapeNet/udfs/02691156/1a04e3eab45ca15dd86060f189eb133/udf"
 
     udf_generator = UDFGenerator(mesh_file_path)
     udf_generator.generateUDF(udf_save_file_basepath)
+
+    udf_file_name_list = os.listdir(udf_save_file_basepath[:-3])
+    print(udf_file_name_list)
     return True
