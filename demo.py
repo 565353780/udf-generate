c7d10bd1010db8f4d31d64b8efc20ecb5a22d1d9
chLi <565353780@qq.com>
2022-10-16 23:25:32 +0800

    add render demo func

--- demo.py
+++ demo.py
@@ -3,7 +3,9 @@
 
 from udf_generate.Demo.udf_generator import demo as demo_generate_udf
 from udf_generate.Demo.udf_generate_manager import demo as demo_generate_udf_folder
+from udf_generate.Demo.render import demo as demo_render
 
 if __name__ == "__main__":
     #  demo_generate_udf()
-    demo_generate_udf_folder()
+    #  demo_generate_udf_folder()
+    demo_render()
